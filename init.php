cddcdc29e63314a4a67b5e0ec04a7a40ee874dbb
Vaibhav Yadav <vaibhavyadav@surmountsoft.in>
2020-07-17 10:59:34 +0530

    bug and permission resolved

--- init.php
+++ init.php
@@ -16,11 +16,11 @@ require "LinkedIn.php";
 
 use myPHPnotes\LinkedIn;
 
-$app_id = "86qrgtb9bw6id0";  // app id from linkedin dev app
-$app_secret = "1zmtGNNNPWyp4yhr"; // app seceret app id from linkedin dev app
+$app_id = "";  // app id from linkedin dev app
+$app_secret = ""; // app seceret app id from linkedin dev app
 
 $app_callback = "http://localhost:85/callback.php";  // path for call back file
-$app_scopes = "r_emailaddress r_liteprofile w_member_social ";
+$app_scopes = "r_emailaddress r_liteprofile w_member_social "; // permissions 
 
 $ssl = false; // ALWAYS TRUE FOR PRODUCTION USE
 
